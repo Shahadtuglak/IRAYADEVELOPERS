33a78a73ce1df15c937bacf3868f4cebceb495d4
Shahad Tuglak <tuglakshahad@gmail.com>
2021-08-14 21:47:25 +0530

    upload javascript file

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+	//Dark Mode
+	$("#dark").click(function(){
+		$("body").css("background-color","black").css("color","white");
+	})
+
+
+	//Light Mode
+	$("#light").click(function(){
+		$("body").css("background-color","white").css("color","black");
+	})
+})
+
